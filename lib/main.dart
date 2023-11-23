6234a2c03bc60d0b313ee935b192394116204d87
IgorGui15 <107226172+IgorGui15@users.noreply.github.com>
2023-11-23 14:38:45 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'view/telalogin.dart';
+import 'view/minhasreceitas.dart'; // Substitua pelo nome real do seu arquivo MyRecipesPage
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Receitas Culinarias',
-      home: LoginPage(),
-    ),
-  );
+      title: 'Receitas Culinárias',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: AuthenticationWrapper(),
+    );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasData) {
+          return MyRecipesPage(); // Substitua pelo nome real do seu arquivo MyRecipesPage
+        } else {
+          return LoginPage();
+        }
+      },
+    );
+  }
 }
 
