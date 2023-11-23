0fea608ed7cce19b9b96dc0ca0e63a60ae244579
IgorGui15 <107226172+IgorGui15@users.noreply.github.com>
2023-11-23 15:00:31 -0300

    Update telacadastro.dart

--- lib/view/telacadastro.dart
+++ lib/view/telacadastro.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignupPage extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,32 +13,64 @@ class SignupPage extends StatelessWidget {
         title: Text('Cadastro de Usuário'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Nome'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'E-mail'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Senha'),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              child: Text('Cadastrar'),
-              onPressed: () {},
-            ),
-            ElevatedButton(
-              child: Text('Já tem uma conta? Faça login'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: 'Nome'),
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'E-mail'),
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Senha'),
+                obscureText: true,
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                child: Text('Cadastrar'),
+                onPressed: () async {
+                  try {
+                    // Adicione a lógica para criar um novo usuário
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                      email: emailController.text.trim(),
+                      password: passwordController.text,
+                    );
+
+                    // Após o cadastro com sucesso, navegue para a próxima tela (por exemplo, a tela principal)
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainPage()),
+                    );
+                  } catch (e) {
+                    // Trate possíveis erros durante o cadastro
+                    print('Erro ao cadastrar usuário: $e');
+                    // Exemplo de exibição de erro para o usuário
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Erro ao cadastrar usuário. Verifique suas informações.'),
+                      ),
+                    );
+                  }
+                },
+              ),
+              SizedBox(height: 16),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Já tem uma conta? Faça login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
